9ef03d33358b0a0860ddcafcb9ffc35f68e65672
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-03-11 16:22:11 +0200

    Update usings

--- Scaffolder.API/Controllers/SystemController.cs
+++ Scaffolder.API/Controllers/SystemController.cs
@@ -3,9 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Scaffolder.API.Application;
-using System.Diagnostics;
-using System.IO;
 using Scaffolder.Core.Engine;
+using System.IO;
 
 namespace Scaffolder.API.Controllers
 {
